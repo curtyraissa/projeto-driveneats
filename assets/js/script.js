531e1017e658371b458b9a7877a854653e1699ff
CurtyRaissa <raissacurty@gmail.com>
2023-01-07 01:47:31 -0300

    seleção de pedidos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-function selecionarPrato(){
-    
+function getPrato(prato) {
+    const selecionado = document.querySelector("#pratos .green-box")
+    if(selecionado !== null){
+        selecionado.classList.remove("green-box")
+    }
+    prato.classList.add("green-box")
+}
 
+
+function getBebida(bebida) {
+    const selecionado = document.querySelector("#bebidas .green-box")
+    if(selecionado !== null){
+        selecionado.classList.remove("green-box")
+    }
+    bebida.classList.add("green-box")
+}
+
+
+function getSobremesa(sobremesa) {
+    const selecionado =  document.querySelector("#sobremesas .green-box")
+    if(selecionado !== null){
+        selecionado.classList.remove("green-box")
+    }
+    sobremesa.classList.add("green-box")
 }
