2cc38c30588cf71dad3179028810cc7e97da2114
CurtyRaissa <raissacurty@gmail.com>
2023-01-16 23:30:13 -0300

    inclusão dos requisitos no README.md

--- README.md
+++ README.md
@@ -4,6 +4,38 @@
 
 <p align="justify">Esse desafio, foi desenvolver a aplicação do DrivenEats a partir de um layout, colocando em prática tudo o que aprendi.</p>
 <hr/>
+✅ Requisitos
+
+- Versionamento do código
+    - [x]  Versionamento usando Git é obrigatório, crie um **repositório público** no seu perfil do GitHub.
+    - [x]  Faça *commits* a cada funcionalidade implementada.
+- *Layout*
+    - [x]  Aplicar layout para *mobile*, seguindo o Figma fornecido (não é necessário implementar um *layout* para *desktop*).
+    - [x]  O topo e o botão de fechar pedido devem ter posicionamento fixo e não rolar com a barra de rolagem.
+    - [x]  Você pode usar imagens e textos aleatórios para ilustrar a página, porém varie o preço em cada item da mesma categoria.
+    - [x]  As fontes utilizadas são: [Righteous](https://fonts.google.com/specimen/Righteous) (nome do restaurante e títulos das categorias) e [Roboto](https://fonts.google.com/specimen/Roboto) (demais textos).
+    - [x]  Para fazer as sombras presentes em alguns elementos, pesquise sobre como aplicar sombras em CSS e brinque com os valores até ficar parecido com as imagens do *layout*.
+    - [x]  Para fazer os produtos rolarem horizontalmente, utilize `overflow-x: scroll`. No modo desktop um *scroll* é exibido, mas no modo celular não.
+- Seleção de itens
+    - [x]  Ao clicar sobre um item, ele deve ser marcado como selecionado.
+    - [x]  Ao clicar em um item, caso já exista um item selecionado na mesma categoria, este deve ser desmarcado e o novo item clicado deve ser o novo selecionado.
+    - [x]  Ao clicar em um item já marcado, não é necessário desmarcá-lo.
+- Botão de finalizar pedido
+    - [x]  Por padrão, o botão de finalizar pedido deve vir desabilitado. Ao clicar no botão nesse estado, nada deve acontecer.
+        - Além de alterar o estilo, use o atributo **disabled** no HTML da tag button.
+    - [x]  Quando o usuário tiver selecionado os itens das três categorias, o botão deve mudar para o estado de habilitado.
+- Envio do pedido
+    - [x]  Ao finalizar o pedido, o usuário deverá ser encaminhado para o WhatsApp Web, em conversa com o contato do restaurante, já com uma mensagem padrão preenchida.
+    - [x]  Essa mensagem deverá seguir este formato, o padrão abaixo:
+        
+        ```
+        Olá, gostaria de fazer o pedido:
+        - Prato: Frango Yin Yang
+        - Bebida: Coquinha Gelada
+        - Sobremesa: Pudim
+        Total: R$ 27,70
+        ```
+<hr/>
 
 ## 🛠 &nbsp;Skills
 <div align="center">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,67 @@
 function getPrato(prato) {
-    const selecionado = document.querySelector("#pratos .green-box")
-    if (selecionado !== null) {
-        selecionado.classList.remove("green-box")
-    }
-    prato.classList.add("green-box")
-    activeButton()
+  const selecionado = document.querySelector("#pratos .green-box")
+  if (selecionado !== null) {
+    selecionado.classList.remove("green-box")
+  }
+  prato.classList.add("green-box")
+  activeButton()
 }
 
 
 function getBebida(bebida) {
-    const selecionado = document.querySelector("#bebidas .green-box")
-    if (selecionado !== null) {
-        selecionado.classList.remove("green-box")
-    }
-    bebida.classList.add("green-box")
-    activeButton()
+  const selecionado = document.querySelector("#bebidas .green-box")
+  if (selecionado !== null) {
+    selecionado.classList.remove("green-box")
+  }
+  bebida.classList.add("green-box")
+  activeButton()
 }
 
 
 function getSobremesa(sobremesa) {
-    const selecionado = document.querySelector("#sobremesas .green-box")
-    if (selecionado !== null) {
-        selecionado.classList.remove("green-box")
-    }
-    sobremesa.classList.add("green-box")
-    activeButton()
+  const selecionado = document.querySelector("#sobremesas .green-box")
+  if (selecionado !== null) {
+    selecionado.classList.remove("green-box")
+  }
+  sobremesa.classList.add("green-box")
+  activeButton()
 }
 
 function activeButton() {
-    let quantidade = document.getElementsByClassName("green-box").length
-    if (quantidade === 3) {
-        document.querySelector("button").classList.add("botao-enviar")
-        document.querySelector("button p").classList.add("fechar-pedido")
-        document.querySelector("button p").innerHTML = "Fechar pedido"
-        document.querySelector("button").removeAttribute("disabled")
-    }
+  let quantidade = document.getElementsByClassName("green-box").length
+  if (quantidade === 3) {
+    document.querySelector("button").classList.add("botao-enviar")
+    document.querySelector("button p").classList.add("fechar-pedido")
+    document.querySelector("button p").innerHTML = "Fechar pedido"
+    document.querySelector("button").removeAttribute("disabled")
+  }
 }
 
 function enviarPedido() {
-    const prato = document.querySelector("#pratos .green-box h4").innerHTML;
-    const bebida = document.querySelector("#bebidas .green-box h4").innerHTML;
-    const sobremesa = document.querySelector(
-      "#sobremesas .green-box h4"
-    ).innerHTML;
-  
-    const precoPrato = document.querySelector("#pratos .green-box .preco").innerHTML;
-    const precoBebida = document.querySelector("#bebidas .green-box .preco").innerHTML;
-    const precoSobremesa = document.querySelector(
-      "#sobremesas .green-box .preco"
-    ).innerHTML;
-  
-    const precoPratoNumber = valores(precoPrato);
-    const precoBebidaNumber = valores(precoBebida);
-    const precoSobremesaNumber = valores(precoSobremesa);
-    const valorTotal = precoPratoNumber + precoBebidaNumber + precoSobremesaNumber;
-  
-    const mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${prato} \n- Bebida: ${bebida} \n- Sobremesa: ${sobremesa}\nTotal: R$ ${valorTotal.toFixed(2)}`
-  
-    const wApp = "https://wa.me/5521993466695?text=" + encodeURI(mensagem);
-    window.open(wApp);
-  }
-  
-  function valores(string) {
-    let numero = string.replace(/[^0-9]/g, "");
-    return parseInt(numero) / 100;
-  }
+  const prato = document.querySelector("#pratos .green-box h4").innerHTML;
+  const bebida = document.querySelector("#bebidas .green-box h4").innerHTML;
+  const sobremesa = document.querySelector(
+    "#sobremesas .green-box h4"
+  ).innerHTML;
 
+  const precoPrato = document.querySelector("#pratos .green-box .preco").innerHTML;
+  const precoBebida = document.querySelector("#bebidas .green-box .preco").innerHTML;
+  const precoSobremesa = document.querySelector(
+    "#sobremesas .green-box .preco"
+  ).innerHTML;
 
+  const precoPratoNumber = valores(precoPrato);
+  const precoBebidaNumber = valores(precoBebida);
+  const precoSobremesaNumber = valores(precoSobremesa);
+  const valorTotal = precoPratoNumber + precoBebidaNumber + precoSobremesaNumber;
+
+  const mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${prato} \n- Bebida: ${bebida} \n- Sobremesa: ${sobremesa}\nTotal: R$ ${valorTotal.toFixed(2)}`
+
+  const wApp = "https://wa.me/5521993466695?text=" + encodeURI(mensagem);
+  window.open(wApp);
+}
+
+function valores(string) {
+  let numero = string.replace(/[^0-9]/g, "");
+  return parseInt(numero) / 100;
+}
